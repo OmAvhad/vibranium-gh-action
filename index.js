533edb74c85954e9e8b06992a97b94140dbd9c80
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-09-01 14:33:37 +0530

    changed import

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const core = require('@actions/core');
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
+import { getInput, setOutput, setFailed } from '@actions/core';
+import { post } from 'axios';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 async function run() {
   try {
     // Get the input parameters
-    const filePath = core.getInput('file-path') || './openapi.json';
-    const apiUrl = core.getInput('api-url') || 'https://vibranium-api-guard.onrender.com/api/endpoints/ingest';    // Resolve the file path
-    const absoluteFilePath = path.resolve(filePath);
+    const filePath = getInput('file-path') || './openapi.json';
+    const apiUrl = getInput('api-url') || 'https://vibranium-api-guard.onrender.com/api/endpoints/ingest';    // Resolve the file path
+    const absoluteFilePath = resolve(filePath);
 
     // Read the JSON file
-    const fileData = fs.readFileSync(absoluteFilePath, 'utf8');
+    const fileData = readFileSync(absoluteFilePath, 'utf8');
     const jsonData = JSON.parse(fileData);
 
     // Make the API call
-    const response = await axios.post(apiUrl, jsonData, {
+    const response = await post(apiUrl, jsonData, {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -26,10 +26,10 @@ async function run() {
     console.log(`Response Data: ${JSON.stringify(response.data)}`);
     
     // Set the output
-    core.setOutput('response-status', response.status);
-    core.setOutput('response-data', JSON.stringify(response.data));
+    setOutput('response-status', response.status);
+    setOutput('response-data', JSON.stringify(response.data));
   } catch (error) {
-    core.setFailed(`Action failed with error: ${error.message}`);
+    setFailed(`Action failed with error: ${error.message}`);
   }
 }
 
